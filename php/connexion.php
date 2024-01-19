896daa3cc3115b37c049dd5dd32420c14074f741
Cédric Longuet <cedric.longuet@etu.iut-tlse3.fr>
2024-01-19 20:14:03 +0100

    update

--- php/connexion.php
+++ php/connexion.php
@@ -6,14 +6,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $login = $_POST["login"];
     $password = $_POST["password"];
     // Vérifier si le login et le mot de passe sont corrects
-    if ($login == "344098" && $password == "$iutinfo") {
+    if ($login == "344098" && $password == "\$iutinfo") {
         // Rediriger l'utilisateur vers la page d'accueil
         setcookie("login", $login, time() + 3600); // Expire dans 1 heure
         setcookie("password", $password, time() + 3600); // Expire dans 1 heure
         header('Location: ../html/secretariat.html');
     } else {
+        echo "erreur";
         // Rediriger l'utilisateur vers la page de connexion
         header('Location: ../index.html');
+        
     }   
 }
 ?>
